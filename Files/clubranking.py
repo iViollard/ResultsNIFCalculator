5b3247cfabf40b57244582215a2cafa9858169b9
iViollard <christopher@j4gdesign.co.uk>
2018-12-14 18:35:37 +0000

    finished mvp
    working version

--- Files/clubranking.py
+++ Files/clubranking.py
@@ -3,12 +3,10 @@ import xlrd
 import requests
 from openpyxl.styles import Color, PatternFill, Font, Border
 from openpyxl.styles import colors
-from apiclient.discovery import build
-from google.auth import credentials
 
 cellBGReset = PatternFill(start_color='FFFFFF',
-                   end_color='FFFFFF',
-                   fill_type='solid')
+                          end_color='FFFFFF',
+                          fill_type='solid')
 cellClrReset = Font(color='000000')
 
 # loads the workbook - assumes the sussex.xlsx file is in the same directory as the script
@@ -17,43 +15,46 @@ wb = openpyxl.load_workbook("sussex.xlsx")
 # load first worksheet
 ws = wb.worksheets[0]
 
-#downloading the relevant files
+# downloading the relevant files
 
-#rankings first
+# rankings first
 
-#go to the British Fencing Association website and download the rankings file (specified)
+# go to the British Fencing Association website and download the rankings file (specified)
 url = "https://www.britishfencing.com/wp-content/uploads/2018/10/mf_oct_2018.xls"
 downloaded_file = requests.get(url)
 
-#write the contents to a new file called rankings.xls
+# write the contents to a new file called rankings.xls
 with open("rankings.xls", 'wb') as file:
     file.write(downloaded_file.content)
 
 # Use xlrd to open older style .xls workbook
 rank_wb = xlrd.open_workbook('rankings.xls')
 
-#go to the iViollard website and download the multipliers file (specified)
+# go to the iViollard website and download the multipliers file (specified)
 url = "http://www.cmollard.co.uk/multipliers.xls"
 downloaded_file = requests.get(url)
 
-#write the contents to a new file called multipliers.xls
+# write the contents to a new file called multipliers.xls
 with open("multipliers.xls", 'wb') as file:
     file.write(downloaded_file.content)
 
 # Use xlrd to open older style .xls workbook
 multipliers_wb = xlrd.open_workbook('multipliers.xls')
 
-# Get the first sheet of the ranking workbook
+# Get the first sheet of each workbook
 rank_ws = rank_wb.sheet_by_index(0)
+multipliers_ws = multipliers_wb.sheet_by_index(0)
 
 # Get the total number of rows to be used to create our license list
 rows = rank_ws.nrows
+rows_lic = multipliers_ws.nrows
 
 # Due to formatting, real numbers don't start until here
 startrow = 5
 
-# Create list of lic numbers
+# Create lists of lic numbers
 rank_lic = rank_ws.col_values(colx=4, start_rowx=startrow, end_rowx=rows)
+mult_lic = multipliers_ws.col_values(colx=0, start_rowx=1, end_rowx=rows_lic)
 
 # print the values in the second column of the first sheet
 for row in ws['B1:B{}'.format(ws.max_row)]:
@@ -63,6 +64,9 @@ for row in ws['B1:B{}'.format(ws.max_row)]:
 # Putting values in same row as "Rank, Name, First name,...." adjust as necessary
 ws.cell(2, 7).value = 'Fencer NIF'
 ws.cell(2, 8).value = 'Points scored'
+
+nif_total = "=SUM(G3:G{})".format(ws.max_row - 1)
+nif_total_list = []
 
 
 # Define function to lookup NIF and return value
@@ -75,25 +79,43 @@ def get_nif(x):
             except ValueError:
                 pass
         startrow = startrow + 1
-    
+
+
 for row in ws['D3:D{}'.format(ws.max_row)]:
     for cell in row:
         nif_val = get_nif(cell.value)
         ws.cell(cell.row, 7).value = nif_val
-        
-#add a sample of text to each cell in the NIF column - needs to be updated with the formula
-        
-for row in ws['H3:H{}'.format(ws.max_row)]:
+        if nif_val is not None:
+            nif_total_list.append(nif_val)
+
+
+# Define function to lookup multiplier and return value
+
+def get_mult(x):
+    startrow = 1
+    for y in mult_lic:
+        if int(x) == y:
+            try:
+                return float(multipliers_ws.cell_value(startrow, 1))
+            except ValueError:
+                pass
+        startrow = startrow + 1
+
+
+for row in ws['A3:A{}'.format(ws.max_row)]:
     for cell in row:
-        ws.cell(cell.row, 8).value = "test"
-        
-#sum of NIF values
+        mult_val = get_mult(cell.value)
+        ws.cell(cell.row, 8).value = int(mult_val) * sum(nif_total_list)
+        print('mult_val for ' + str(cell.value) + ' is ' + str(mult_val))
 
-Grand_Total_Row = ws.max_row + 1 
-ws.cell(Grand_Total_Row, 1).value = "Grand Total"
-ws.cell(Grand_Total_Row, 7).value = "=SUM(G3:G{})".format(ws.max_row - 1)
+# sum of NIF values
 
-#styles - currently setting top row to black text on white bg
+Grand_Total_Row = ws.max_row + 1
+grand_total = "Grand total"
+ws.cell(Grand_Total_Row, 1).value = grand_total
+ws.cell(Grand_Total_Row, 7).value = nif_total
+
+# styles - currently setting top row to black text on white bg
 
 ws['A1'].fill = cellBGReset
 ws['A1'].font = cellClrReset
--- README.md
+++ README.md
@@ -4,11 +4,3 @@ To run the Python scripts:
 1 open terminal
 2 change directory (cd) to the relevant folder
 3 run python test.py
-
-What it does so far:
-
-- gets latest ranking file from BFA website
-- gets multipliers for www.cmollard.co.uk
-- write both to new files
-- creates a new results file
-- in the new results file: gets NIF value for each fencer and lists it; sums them; adds a new column with "test" in each cell
